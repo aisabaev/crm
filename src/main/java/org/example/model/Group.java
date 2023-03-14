da557552ac740fee7dc07c59ee5fdf4c4e93fb58
techiboush <techiboush@gmail.com>
2023-03-14 19:38:03 +0600

    Добавлены поля для класса Group согласно условию

--- src/main/java/org/example/model/Group.java
+++ src/main/java/org/example/model/Group.java
@@ -1,7 +1,19 @@
 package org.example.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalTime;
+@Getter
+@Setter
+@ToString
+
 public class Group extends BaseEntity{
 
-    String groupName;
+    private String name;
+    private LocalTime groupTime;
+    private Course course;
+    private Mentor mentor;
     
 }
